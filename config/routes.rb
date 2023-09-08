b8aba39e2453e45b5b305dc3b2b0ccf767661816
Marc Edwards <dyfroedd@gmail.com>
2023-09-09 00:06:13 +0100

    added about page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   #get 'home/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get 'home/about'
 end
